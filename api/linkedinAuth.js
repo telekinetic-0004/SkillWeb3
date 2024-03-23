49b30df5fb4ff0b81894b4d5a7b759003e10de1b
telekinetic-0004 <jayminschandaria@gmail.com>
2024-03-23 17:40:13 +0530

    third commit

---
+++ api/linkedinAuth.js
@@ -0,0 +1,38 @@
+// linkedinAuth.js
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
+    const { code, state } = req.query;
+
+    if (code) {
+        try {
+            const tokenResponse = await fetch('https://www.linkedin.com/oauth/v2/accessToken', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: `grant_type=authorization_code&code=${code}&client_id=${process.env.LINKEDIN_CLIENT_ID}&client_secret=${process.env.LINKEDIN_CLIENT_SECRET}&redirect_uri=${process.env.LINKEDIN_REDIRECT_URI}`
+            });
+
+            const { access_token } = await tokenResponse.json();
+
+            // Redirect the user back to your frontend with the access token
+            res.writeHead(302, {
+                Location: `YOUR_FRONTEND_URL?access_token=${access_token}&state=${state}`
+            });
+            res.end();
+        } catch (error) {
+            res.status(500).send('Server Error');
+        }
+    } else {
+        // Redirect the user to LinkedIn's authorization page
+        const scope = 'r_liteprofile%20r_emailaddress%20w_member_social';
+        const redirect_uri = process.env.LINKEDIN_REDIRECT_URI;
+        const client_id = process.env.LINKEDIN_CLIENT_ID;
+        const state = 'RANDOM_STRING';
+        const url = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&state=${state}&scope=${scope}`;
+
+        res.writeHead(302, { Location: url });
+        res.end();
+    }
+};
--- assets/script_common.js
+++ assets/script_common.js
@@ -23,7 +23,7 @@ async function connectWallet() {
 // Function to authenticate with LinkedIn
 function authenticateWithLinkedIn() {
     // Redirect to LinkedIn authentication page
-    window.location.href = '/auth/linkedin';
+    window.location.href = 'https://skill-web3.vercel.app/api/linkedinAuth';
 }
 
 // Function to make payment using MetaMask

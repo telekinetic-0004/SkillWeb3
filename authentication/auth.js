082eeb110755cc51ee6f27ebfb5d2d252c368e25
telekinetic-0004 <jayminschandaria@gmail.com>
2024-03-31 22:49:50 +0530

    40th commit

--- authentication/auth.js
+++ authentication/auth.js
@@ -1,6 +1,6 @@
 (function () {
-    // Define a variable to hold the Auth0Client object
-    let auth0;
+    // Define a variable to hold the Auth0WebAuth object
+    let auth0WebAuth;
 
     // Create a new script tag for the Auth0 library
     const auth0Script = document.createElement('script');
@@ -9,22 +9,33 @@
 
     // Handle the Auth0 library script load event
     auth0Script.onload = function () {
-        // Initialize Auth0Client once the library is loaded
-        auth0 = new Auth0Client({
+        // Initialize Auth0WebAuth once the library is loaded
+        auth0WebAuth = new auth0.WebAuth({
             domain: 'dev-1lhu6wr3urnf83ul.us.auth0.com',
-            client_id: 'jTYAK1RXiJkjjQPDClv2ymDfrcvJrUYv',
-            redirect_uri: 'https://skill-web3.vercel.app/home.html',
+            clientID: 'jTYAK1RXiJkjjQPDClv2ymDfrcvJrUYv',
+            redirectUri: 'https://skill-web3.vercel.app/home.html',
             audience: 'https://dev-1lhu6wr3urnf83ul.us.auth0.com/api/v2/',
+            responseType: 'token id_token',
             scope: 'openid profile email'
         });
 
         // Handle authentication
-        async function handleAuthentication() {
-            const isAuthenticated = await auth0.isAuthenticated();
-            if (isAuthenticated) {
-                // Redirect to home page or perform necessary actions
-                window.location.href = 'https://skill-web3.vercel.app/home.html';
-            }
+        function handleAuthentication() {
+            auth0WebAuth.parseHash(window.location.hash, function (err, result) {
+                if (err) {
+                    console.error("Error during authentication:", err);
+                    return;
+                }
+
+                if (result && result.idToken) {
+                    // Store the access token and id token in local storage
+                    localStorage.setItem('access_token', result.accessToken);
+                    localStorage.setItem('id_token', result.idToken);
+
+                    // Redirect to home page or perform necessary actions
+                    window.location.href = 'https://skill-web3.vercel.app/home.html';
+                }
+            });
         }
 
         handleAuthentication();
@@ -33,8 +44,10 @@
         var googleSignInBtn = document.getElementById('googleSignInBtn');
         googleSignInBtn.addEventListener('click', function () {
             // Trigger Auth0 authentication
-            auth0.loginWithRedirect({
-                connection: 'google'
+            auth0WebAuth.authorize({
+                connection: 'google',
+                responseType: 'token id_token',
+                scope: 'openid profile email'
             });
         });
     };

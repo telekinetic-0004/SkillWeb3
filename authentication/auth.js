1582080a55ea1f002e3a67b572830b9efca60fac
telekinetic-0004 <jayminschandaria@gmail.com>
2024-03-31 22:01:13 +0530

    38th commit

--- authentication/auth.js
+++ authentication/auth.js
@@ -7,11 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
     // Wait for the Auth0 library to load
     auth0Script.onload = function() {
         // Initialize Auth0Client once the library is loaded
-        var auth0 = new Auth0Client({
+        var auth0 = new window.auth0.WebAuth({
             domain: 'dev-1lhu6wr3urnf83ul.us.auth0.com',
-            client_id: 'jTYAK1RXiJkjjQPDClv2ymDfrcvJrUYv',
-            redirect_uri: 'https://skill-web3.vercel.app/home.html',
+            clientID: 'jTYAK1RXiJkjjQPDClv2ymDfrcvJrUYv',
+            redirectUri: 'https://skill-web3.vercel.app/home.html',
             audience: 'https://dev-1lhu6wr3urnf83ul.us.auth0.com/api/v2/',
+            responseType: 'token id_token',
             scope: 'openid profile email'
         });
 
@@ -30,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         googleSignInBtn.addEventListener('click', function() {
             // Trigger Auth0 authentication
-            auth0.loginWithRedirect({
+            auth0.authorize({
                 connection: 'google'
             });
         });

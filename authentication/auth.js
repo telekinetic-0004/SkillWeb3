bbaf6b5cce348821e3807d51c16e6a020898941e
telekinetic-0004 <jayminschandaria@gmail.com>
2024-03-31 22:08:36 +0530

    38th commit

--- authentication/auth.js
+++ authentication/auth.js
@@ -7,18 +7,28 @@ document.addEventListener('DOMContentLoaded', function() {
     // Wait for the Auth0 library to load
     auth0Script.onload = function() {
         // Initialize Auth0Client once the library is loaded
-        var auth0 = new window.auth0.WebAuth({
+        var auth0 = new Auth0Client({
             domain: 'dev-1lhu6wr3urnf83ul.us.auth0.com',
-            clientID: 'jTYAK1RXiJkjjQPDClv2ymDfrcvJrUYv',
-            redirectUri: 'https://skill-web3.vercel.app/home.html',
+            client_id: 'jTYAK1RXiJkjjQPDClv2ymDfrcvJrUYv',
+            redirect_uri: 'https://skill-web3.vercel.app/home.html',
             audience: 'https://dev-1lhu6wr3urnf83ul.us.auth0.com/api/v2/',
-            responseType: 'token id_token',
             scope: 'openid profile email'
         });
 
         // Handle authentication
         async function handleAuthentication() {
-            const isAuthenticated = await auth0.isAuthenticated();
+            const isAuthenticated = await new Promise((resolve, reject) => {
+                auth0.checkSession({}, (err, result) => {
+                    if (err) {
+                        // Session not active
+                        resolve(false);
+                    } else {
+                        // Session active
+                        resolve(true);
+                    }
+                });
+            });
+
             if (isAuthenticated) {
                 // Redirect to home page or perform necessary actions
                 window.location.href = 'https://skill-web3.vercel.app/home.html';
@@ -31,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         googleSignInBtn.addEventListener('click', function() {
             // Trigger Auth0 authentication
-            auth0.authorize({
+            auth0.loginWithRedirect({
                 connection: 'google'
             });
         });
